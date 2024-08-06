07f1a09611f1827ce6c9799344cae475572680d6
Anastasiia Tykha <anastasiia.tykha@academysmart.com>
2024-08-06 22:36:55 +0300

    added to process env

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,5 +1,26 @@
 import { Sequelize } from "sequelize";
-import { dataBaseConfig } from "./config.js";
+import "dotenv/config";
+
+const {
+  DATABASE_NAME,
+  DATABASE_DIALECT,
+  DATABASE_USERNAME,
+  DATABASE_PASSWORD,
+  DATABASE_HOST,
+  DATABASE_PORT,
+} = process.env;
+
+export const dataBaseConfig = {
+  username: DATABASE_NAME,
+  dialect: DATABASE_DIALECT,
+  database: DATABASE_USERNAME,
+  password: DATABASE_PASSWORD,
+  host: DATABASE_HOST,
+  port: DATABASE_PORT,
+  dialectOptions: {
+    ssl: true,
+  },
+};
 const sequelize = new Sequelize(dataBaseConfig);
 
 export default sequelize;
